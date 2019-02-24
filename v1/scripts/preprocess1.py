bbe59a5e7cc6852e64d9a676be54b963e2a9f7b4
InfiniteEEMS <wendache@usc.edu>
2019-02-23 22:54:31 -0800

    First Commit

---
+++ v1/scripts/preprocess1.py
@@ -0,0 +1,44 @@
+import os
+import time
+import cv2 as cv
+
+def task(onefile, pathin, pathout):
+
+
+    filelist=[]
+    filelist.append(onefile)
+
+    #store the data&time info
+    snapshot_time = filelist[0].partition('_')[2]
+    time.sleep(10)
+
+
+
+    for filename in filelist:
+
+        # open the target jpeg and convert to gray scale        
+        src = cv.imread(os.path.join(pathin, filename))
+        src = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+        # Histogram Equalization to improve the contrast of image
+        #dst = cv.equalizeHist(src)
+
+        cv.imwrite(os.path.join(pathout,'processed1_'+snapshot_time), src)
+
+    return [os.path.join(pathout,'processed1_'+snapshot_time)]
+
+
+
+def main():
+    filelist= 'camera1_20190222.jpeg'
+    outpath = os.path.join(os.path.dirname(__file__), "generated_files/")
+    outfile = task(filelist, outpath, outpath)
+    return outfile
+
+if __name__ == '__main__':
+
+    #Suppose the file structure is erick/detection_app/camera1_input/camera1_20190222.jpeg
+    filelist= 'camera1_20190222.jpeg'
+    task(filelist, '/home/erick/detection_app/camera1_input', '/home/erick/detection_app')
+
+
