c0ba416fd2761ce566de1631a5767066e4828c8f
InfiniteEEMS <wendache@usc.edu>
2019-04-23 23:31:22 -0700

    update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -1,12 +1,6 @@
 apt-get update
 apt-get -y install build-essential
-apt-get -y install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
+apt-get -y install libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 apt-get -y install python3-dev python-dev python-pip python3-pip
-
-
-pip3 install jasper
-pip3 install numpy
-pip install numpy
 
 pip3 install opencv-contrib-python
